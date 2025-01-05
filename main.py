430c606f3e6b6050864c40303168e89a153f2c62
HoraceC_Dev <choracehc@gmail.com>
2025-01-05 18:15:47 -0500

    update to latest logic

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
+from typing import List
 
 app = FastAPI()
 
@@ -11,30 +12,45 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-class ForexData(BaseModel):
-    data: str
-    current_time: str
+class CandleData(BaseModel):
+    time: int
+    open: float
+    high: float
+    low: float
+    close: float
 
 @app.post("/htf_data/")
-def htf_data(data: ForexData):
+def htf_data(data: List[CandleData]):
     print(data)
     print(type(data))
     return {"message": "Data received"}
 
 @app.post("/mtf_data/")
-def htf_data(data: ForexData):
+def htf_data(data: List[CandleData]):
     print(data)
     print(type(data))
     return {"message": "Data received"}
 
 @app.post("/htf_data/")
-def htf_data(data: ForexData):
+def htf_data(data: List[CandleData]):
     print(data)
     print(type(data))
     return {"message": "Data received"}
 
 @app.post("/trade_decision")
-def htf_data(data: ForexData):
-    print(data)
-    print(type(data))
-    return {"message": "Data received", "trade_decision": "Buy"}
+def htf_data(data: List[CandleData]):
+    
+    # Example: If the last candle's close price is above 1.1300, suggest a "buy"
+    if data[-1].close > 1.1300:
+        trade_decision = True
+        trade_details = {
+            "trade": True,
+            "direction": "buy",
+            "volume": 0.1,
+            "sl": 1.1280,
+            "tp": 1.1350
+        }
+    else:
+        trade_details = {"trade": False}
+
+    return trade_details
